58a2471e9b13bb41d2767f52bbd95598413dca22
Adam Salwowski <zadca123@mailo.com>
2021-07-18 23:18:17 +0200

    updating files

--- z023.py
+++ z023.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python
 
 
-# def if_valid_password(p):
-#     L = p.split(",")
-#     for i in L:
-#         print(L)
-#         if len(i) < 6 or len(i) > 12:
-#             return False
-#         for c in i:
-#             if c not in "qwertyuioplkjhgfdsazxcvbnm":
-#                 pass
-#             elif c not in "0123456789":
-#                 pass
-#             elif c not in "QWERTYUIOPLKJHGFDSAZXCVBNM":
-#                 pass
-#             elif c not in "@#":
-#                 pass
-#             else:
-#                 break
+def if_valid_password(str_pass):
+    # convert string of multiple passwords separated by coma to list
+    pass_list = str_pass.split(",")
+    print(pass_list)
+    for password in pass_list:
+        if len(password) < 6 or len(password) > 12:
+            print(password + "is too short or too long")
+            continue
+        count = 0
+        for c in password:
+            # if c not in small or capital or numbers or special:
+            if c in "qwertyuioplkjhgfdsazxcvbnm":
+                a = 1
+            elif c not in "qwertyuioplkjhgfdsazxcvbnm":
+                b = 1
+            elif c not in "0123456789":
+                c = 1
+            elif c not in "@#":
+                d = 1
+            else:
+                count += 1
+        print(count)
+        if count == len(password):
+            print(password + " is a valid password")
+        else:
+            print(password + " is NOT a valid password")
 
-# for x in "123456789":
-#     for i in "3456":
-#         if i not in "x":
-#             pass
-#         print(i)
 
-# # p = input("Type password: ")
-# p = "ABd1234@1,a F#,2w3E*,2We3345"
-# print(if_valid_password(p))
+# p = input("Type password: ")
+str_pass = "ABd1234@1,a F#,2w3E*,2We3345"
+# print(if_valid_password(str_pass))
+if_valid_password(str_pass)
